1450bbd77227af6d881ec336001f7c3624034429
Giulio955 <99029808+Giulio955@users.noreply.github.com>
2022-02-14 18:18:28 +0100

    Add files via upload

---
+++ progettolarave/app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $search = request()->get('search');
+        $users = User::where('name', 'LIKE', '%'.$search.'%')->get();
+        return $users;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->get('data');
+        $user = User::firstOrCreate(["email" => $data['email']], $data);
+        /*$user = new User($data);
+        $user->save();*/
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       $user = User::find($id);
+       return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->get('data');
+        $user = User::find($id);
+        $user->fill($data)->save();
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $stock = User::find($id);
+        $stock->delete();
+ 
+    }
+}
